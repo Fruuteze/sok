104936f9689145863105ec51efcb57a841e71b2a
Fruuteze <113626939+Fruuteze@users.noreply.github.com>
2023-03-28 22:55:03 +0300

    Add files via upload

---
+++ Con.py
@@ -0,0 +1,9 @@
+# y=ax^2+bx+c
+print('решаем функцию y = a*x**2+b*x+c')
+a = int(input("Введите значение a= "))
+b = int(input("Введите значение b= "))
+c = int(input("Введите значение c= "))
+x = int(input('Введите значение x= '))
+
+function = y=a*x**2+b*x+c
+print('y = ' + str(function))
--- next.py
+++ next.py
@@ -1,10 +1,19 @@
-x = int(input('Введите значение x = '))
+print('Решаем уравнение a*x**2+b*x+c=0')
+a = int(input('Введите значение a: '))
+b = int(input('Введите значение b: '))
+c = int(input('Введите значение c: '))
 
-if x > 0:
-    y = 2 * x - 10
-elif x == 0:
-    y = 0
-if x < 0:
-    y = 2 * x - 1
+discr = b**2 - 4*a*c
+print('Дискриминант = ' + str(discr))
+if discr < 0:
+    print('Корней нет')
+elif discr == 0:
+    x = -b / (2 * a)
+    print('x = ' + str(x))
+else:
+    x1 = (-b + discr ** 0.5) / (2 * a)
+    x2 = (-b - discr ** 0.5) / (2 * a)
+    print('x1 = ' + str(x1))
+    print('x2 = ' + str(x2))
 
-print(' y = ', y)
+
